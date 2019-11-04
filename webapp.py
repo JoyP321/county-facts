85f2c5b38a396b2f328444c736350451cf72700d
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2019-11-04 09:36:08 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, Markup, render_template, flash, Markup
 import os
 import json
-first = 2
 app = Flask(__name__)
 
 @app.route("/")
+first =2
 def render_main(): 
-  first=first-1
+  first = first-1
   with open('county_demographics.json') as demographics_data:
     counties = json.load(demographics_data)
   if first <= 0:
@@ -29,3 +29,4 @@ def get_fun_fact(counties, state):
 
 if __name__=="__main__":
     app.run(debug=False)
+    
