ab2df1684bf0f5ce6d2a98ed6483a2002b16da5e
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2019-11-05 09:49:24 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -14,7 +14,7 @@ def render_main():
 def render_main2(): 
   with open('county_demographics.json') as demographics_data:
     counties = json.load(demographics_data)
-  randomVal = int(random()*2)
+  randomVal = int(random.random()*2)
   if randomVal == 0:
     return render_template('home.html', options = get_state_options(counties), reply = get_fun_fact1(counties, request.args['state']))
   else:
