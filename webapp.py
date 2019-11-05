4506b547dded5e28f1775b5e56e0c0faca0d96d3
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2019-11-05 10:01:19 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def render_main(): 
   with open('county_demographics.json') as demographics_data:
     counties = json.load(demographics_data)
-  return render_template('home.html', options = get_state_options(counties), reply = "hey")
+  return render_template('home.html', options = get_state_options(counties), reply = "")
 
 @app.route("/reply")
 def render_main2(): 
