53cdba3536e90985c0d78ca94f58fb3cba049f6d
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2019-11-07 09:51:37 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -66,7 +66,7 @@ def get_state_fact3(counties, state):
   return fact
 
 def get_county_fact1(counties, county):
-  print(county['Housing']['Households'])
+  print(counties[county]['Housing']['Households'])
   return "o"
   
 
