c69a8f276dbeada49fb5c27887952ea29b4da4f7
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2019-11-07 09:23:41 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def render_main(): 
   with open('county_demographics.json') as demographics_data:
     counties = json.load(demographics_data)
-  return render_template('home.html', options = get_state_options(counties), reply = "")
+  return render_template('home.html', options = get_state_options(counties), reply = "", options2 = get_county_options(counties), reply2 = "")
 
 @app.route("/reply")
 def render_main2(): 
@@ -16,11 +16,11 @@ def render_main2():
     counties = json.load(demographics_data)
   randomVal = int(random.random()*3)
   if randomVal == 0:
-    return render_template('home.html', options = get_state_options(counties), reply = get_fun_fact1(counties, request.args['state']))
+    return render_template('home.html', options = get_state_options(counties), reply = get_fun_fact1(counties, request.args['state']), options2 = get_county_options(counties), reply2 = "hh"))
   elif randomVal == 1:
-    return render_template('home.html', options = get_state_options(counties), reply = get_fun_fact2(counties, request.args['state']))
+    return render_template('home.html', options = get_state_options(counties), reply = get_fun_fact2(counties, request.args['state']), options2 = get_county_options(counties), reply2 = "hh")
   else:
-    return render_template('home.html', options = get_state_options(counties), reply = get_fun_fact3(counties, request.args['state']))
+    return render_template('home.html', options = get_state_options(counties), reply = get_fun_fact3(counties, request.args['state']), options2 = get_county_options(counties), reply2 = "hh")
 
 def get_state_options(counties):
   listOfStates = []
@@ -30,6 +30,15 @@ def get_state_options(counties):
   options=""
   for state in listOfStates:
     options = options + Markup("<option value=\"" + state + "\">" + state + "</option>")
+  return options
+
+def get_county_options(counties):
+  listOfCounties = []
+  for county in counties:
+    listOfCounties.append(county)
+  options=""
+  for county in listOfCounties:
+    options = options + Markup("<option value=\"" + county + "\">" + county + "</option>")
   return options
 
 def get_fun_fact1(counties, state):
