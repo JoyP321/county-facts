42c575cc3eb6d2790b9756bae67afc7769216629
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2019-11-08 09:43:20 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -22,7 +22,7 @@ def render_second_dropdown():
     stateFact = get_state_fact2(counties, request.args['state'])
   else:
     stateFact = get_state_fact2(counties, request.args['state'])
-    
+   print("good rn")
   return render_template('home.html', options = get_state_options(counties), reply = Markup("<p>" + stateFact + "</p>"), options2 = get_county_options(counties, request.args['state']), reply2 = "")
            
 @app.route("/reply2")
